ebe0e0aa7557fb50f3920ca09c9632c7913c669c
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-04-30 08:47:02 +0545

    db models

---
+++ backend/internal/models/user.go
@@ -0,0 +1,31 @@
+package models
+
+import "time"
+
+type User struct {
+	ID    string `json:"id"`
+	Email string `json:"email"`
+}
+
+type Token struct {
+	UserID       string    `json:"user_id"`
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token"`
+	Expiry       time.Time `json:"expiry"`
+	SessionID    string    `json:"session_id"`
+}
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+type UserBook struct {
+	UserID string `json:"user_id"`
+	BookID string `json:"book_id"`
+	// use pointers for times so they can be "nil" if the book isn't started/finished
+	StartedAt  *time.Time `json:"started_at"`
+	FinishedAt *time.Time `json:"finished_at"`
+	Rating     int        `json:"rating"`
+	ReadStatus int        `json:"rstatus"` // 0 = not started, 1 = reading, 2 = finished
+}
